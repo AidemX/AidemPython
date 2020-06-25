e45aa7fcd0b9b99286747e8c83b3b135f47c69de
Kjuly <dev@kjuly.com>
2020-06-25 17:49:08 +0800

    VMPython/VMPython.m: Add load_custom_builtin_importer()

--- PythonForVideoMemos/VMPython/VMPython.m
+++ PythonForVideoMemos/VMPython/VMPython.m
@@ -32,6 +32,67 @@
   return _sharedVMPython;
 }
 
+#pragma mark - Private
+
+void load_custom_builtin_importer() {
+  static const char *custom_builtin_importer = \
+  "import sys, imp, types\n" \
+  "from os import environ\n" \
+  "from os.path import exists, join\n" \
+  "try:\n" \
+  "    # python 3\n"
+  "    import _imp\n" \
+  "    EXTS = _imp.extension_suffixes()\n" \
+  "    sys.modules['subprocess'] = types.ModuleType(name='subprocess')\n" \
+  "    sys.modules['subprocess'].PIPE = None\n" \
+  "    sys.modules['subprocess'].STDOUT = None\n" \
+  "    sys.modules['subprocess'].DEVNULL = None\n" \
+  "    sys.modules['subprocess'].CalledProcessError = Exception\n" \
+  "    sys.modules['subprocess'].check_output = None\n" \
+  "except ImportError:\n" \
+  "    EXTS = ['.so']\n"
+  "# Fake redirection to supress console output\n" \
+  "if environ.get('KIVY_NO_CONSOLE', '0') == '1':\n" \
+  "    class fakestd(object):\n" \
+  "        def write(self, *args, **kw): pass\n" \
+  "        def flush(self, *args, **kw): pass\n" \
+  "    sys.stdout = fakestd()\n" \
+  "    sys.stderr = fakestd()\n" \
+  "# Custom builtin importer for precompiled modules\n" \
+  "class CustomBuiltinImporter(object):\n" \
+  "    def find_module(self, fullname, mpath=None):\n" \
+  "        # print(f'find_module() fullname={fullname} mpath={mpath}')\n" \
+  "        if '.' not in fullname:\n" \
+  "            return\n" \
+  "        if not mpath:\n" \
+  "            return\n" \
+  "        part = fullname.rsplit('.')[-1]\n" \
+  "        for ext in EXTS:\n" \
+  "           fn = join(list(mpath)[0], '{}{}'.format(part, ext))\n" \
+  "           # print('find_module() {}'.format(fn))\n" \
+  "           if exists(fn):\n" \
+  "               return self\n" \
+  "        return\n" \
+  "    def load_module(self, fullname):\n" \
+  "        f = fullname.replace('.', '_')\n" \
+  "        mod = sys.modules.get(f)\n" \
+  "        if mod is None:\n" \
+  "            # print('LOAD DYNAMIC', f, sys.modules.keys())\n" \
+  "            try:\n" \
+  "                mod = imp.load_dynamic(f, f)\n" \
+  "            except ImportError:\n" \
+  "                # import traceback; traceback.print_exc();\n" \
+  "                # print('LOAD DYNAMIC FALLBACK', fullname)\n" \
+  "                mod = imp.load_dynamic(fullname, fullname)\n" \
+  "            sys.modules[fullname] = mod\n" \
+  "            return mod\n" \
+  "        return mod\n" \
+  "sys.meta_path.insert(0, CustomBuiltinImporter())";
+  PyRun_SimpleString(custom_builtin_importer);
+}
+
+#pragma mark - Public
+
 - (void)enterPythonEnv
 {
   if (self.isInitialized) {
@@ -96,6 +157,8 @@
   NSLog(@"Python Environment Initialization %@", (pythonEnvInitialized ? @"Succeeded" : @"Failed"));
   
   self.initialized = YES;
+  
+  load_custom_builtin_importer();
 }
 
 - (void)quitPythonEnv
