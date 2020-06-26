5406a241b10c86f75c405443f922e2ad56b35fed
Kjuly <dev@kjuly.com>
2020-06-26 15:18:51 +0800

    ViewController: Add tableview

--- PythonForVideoMemos-Demo/PythonForVideoMemos-Demo/ViewController.m
+++ PythonForVideoMemos-Demo/PythonForVideoMemos-Demo/ViewController.m
@@ -11,7 +11,16 @@
 // Lib
 #import "VMPythonRemoteSourceDownloader.h"
 
-@interface ViewController ()
+
+@interface ViewController () <
+  UITableViewDataSource,
+  UITableViewDelegate
+>
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, copy) NSString *urlString;
+@property (nonatomic, copy) NSArray <VMRemoteSourceOptionModel *> *items;
 
 @property (nonatomic, strong) VMPythonRemoteSourceDownloader *downloader;
 
@@ -25,14 +34,106 @@
   [super viewDidLoad];
   
   // Do any additional setup after loading the view.
+  self.view.backgroundColor = [UIColor blackColor];
+  
+  _tableView = ({
+    UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.opaque = YES;
+    tableView.scrollEnabled = YES;
+    tableView.showsHorizontalScrollIndicator = NO;
+    tableView.showsVerticalScrollIndicator = NO;
+    tableView.bounces = YES;
+    tableView.backgroundView = nil;
+    
+    tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    
+    // TableView Footer (empty view here to hide separators between empty cells).
+    tableView.sectionFooterHeight = 0;
+    tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:tableView];
+    
+    tableView;
+  });
+  
+  [NSLayoutConstraint activateConstraints:
+   @[[_tableView.topAnchor constraintEqualToAnchor:self.view.topAnchor],
+     [_tableView.leftAnchor constraintEqualToAnchor:self.view.leftAnchor],
+     [_tableView.rightAnchor constraintEqualToAnchor:self.view.rightAnchor],
+     [_tableView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor]
+   ]];
   
   NSString *docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).lastObject;
   _downloader = [[VMPythonRemoteSourceDownloader alloc] initWithSavePath:docPath];
-  // Check
-  [_downloader checkWithURLString:@"https://www.bilibili.com/video/BV1kW411p7B3"];
-  // Download
-  //[_downloader downloadWithURLString:@"https://www.bilibili.com/video/BV1kW411p7B3"];
+  
+  self.urlString = @"https://www.bilibili.com/video/BV1kW411p7B3";
+  
+  // Download directly w/ default format
+  //[_downloader downloadWithURLString:self.urlString inFormat:nil];
+  
+  // Check source w/ URL
+  typeof(self) __weak weakSelf = self;
+  [_downloader checkWithURLString:self.urlString completion:^(NSArray *options) {
+    weakSelf.items = options;
+    [weakSelf.tableView reloadData];
+  }];
 }
 
+#pragma mark - UITableViewDataSource
+
+// Asks the data source to return the number of sections in the table view.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+  return 1;
+}
+
+// Tells the data source to return the number of rows in a given section of a table view. (required)
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  return [self.items count];
+}
+
+// Asks the data source for a cell to insert in a particular location of the table view.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  static NSString * const cellIdentifier = @"cell";
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+  if (!cell) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+  }
+  
+  VMRemoteSourceOptionModel *item = self.items[indexPath.row];
+  cell.textLabel.text = item.qualityText;
+  cell.detailTextLabel.text = [NSString stringWithFormat:@"TYPE: %@, SIZE: %@", item.mediaTypeText, item.sizeText];
+  
+  return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+// Tells the delegate the table view is about to draw a cell for a particular row.
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  // In autolayout, cell's subview layout will be displayed animated, let's rm the animation
+  [UIView performWithoutAnimation:^{
+    [cell layoutIfNeeded];
+  }];
+}
+
+// Asks the delegate for the height to use for a row in a specified location.
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  return UITableViewAutomaticDimension;
+}
+
+// Tells the delegate that the specified row is now selected.
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  VMRemoteSourceOptionModel *item = self.items[indexPath.row];
+  [_downloader downloadWithURLString:self.urlString inFormat:item.format];
+}
 
 @end
